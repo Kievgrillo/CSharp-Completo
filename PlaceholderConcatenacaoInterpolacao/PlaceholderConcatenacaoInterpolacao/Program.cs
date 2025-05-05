34ac18234485956b7f656cdfbce9cb0431dda5ca
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-05 16:21:03 -0300

    Add files via upload

---
+++ PlaceholderConcatenacaoInterpolacao/PlaceholderConcatenacaoInterpolacao/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace PlacholderConcatenacaoInterpolacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 32; 
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            //Placeholders
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome , idade, saldo);
+
+            //Interpolacao
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+
+            //Concatenacao
+            Console.WriteLine( nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais ");
+        }
+    }
+}

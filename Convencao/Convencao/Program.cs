4925e6302287429b507419a1ed8957a259975db8
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-05 12:05:51 -0300

    Add files via upload

---
+++ Convencao/Convencao/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Curso
+{
+    //Usando pascal case
+    class ContaBancaria
+    {
+        public string Titular { get; set; }
+        private double _saldo;
+
+        public void Deposito(double quantia)
+        {
+            _saldo += quantia;
+        }
+
+        //usando camel case
+        public double GetSaldo()
+        {
+            //_saldo atributo interno da classe
+            return _saldo;
+        }
+    }
+}

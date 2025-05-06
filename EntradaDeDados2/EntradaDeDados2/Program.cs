b55c60581f41a5ec5ef4d96f82ec5f26b2f927dd
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-06 13:44:04 -0300

    Add files via upload

---
+++ EntradaDeDados2/EntradaDeDados2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace EntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            char B = char.Parse(Console.ReadLine());
+            double C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //para poder aceitar valores com "."
+
+            string[] vet = Console.ReadLine().Split(' '); //vetor 
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture); //para poder aceitar valores com "."
+
+           
+            Console.WriteLine("Numero inteiro :" + A);  //Cancatenando VAR
+            Console.WriteLine("Caractere :" + B);
+            Console.WriteLine("Numero double : " + C);
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2" , CultureInfo.InvariantCulture));
+
+        }
+    }
+}

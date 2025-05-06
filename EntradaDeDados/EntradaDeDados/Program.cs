72c298f5b783b5b82604d2cb68bebb9fd1bc5197
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-06 13:17:37 -0300

    Add files via upload

---
+++ EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            string A = vet[0];
+            string B = vet[1];
+            string C = vet[2];
+
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine(C);
+        }
+    }
+}

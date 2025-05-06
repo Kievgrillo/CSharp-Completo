9af9a5390b8f8cb0977b9f4ffbf2d304e3047577
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-06 12:47:54 -0300

    Add files via upload
    Conversão implícita de variáveis, sempre ficar atento ao tipo de VAR que irá ser convertida, sem perder informação por conta dos bytes

---
+++ UsandoCasting/UsandoCasting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UsandoCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 5;
+            int B = 2;
+
+            //Conversao implicita, fazendo a var A INT ser divisivel por DOUBLE
+            double resultado = (double)A / B;
+
+            Console.WriteLine(resultado);
+        }
+    }
+}

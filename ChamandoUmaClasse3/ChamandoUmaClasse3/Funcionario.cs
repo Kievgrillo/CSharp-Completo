4a42abba9e75d9341e4298055c19061a9afe39a3
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-09 22:39:22 -0300

    Add files via upload

---
+++ ChamandoUmaClasse3/ChamandoUmaClasse3/Funcionario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChamandoUmaClasse3
+{
+    internal class Funcionario
+    {
+        public String Nome;
+        public double Salario;
+    }
+}
---
+++ ChamandoUmaClasse3/ChamandoUmaClasse3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ChamandoUmaClasse3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario A1 = new Funcionario();
+            Funcionario A2 = new Funcionario();
+
+            Console.WriteLine("Digite os dados do primeiro funcionario :");
+            Console.Write("Nome :");
+            A1.Nome = Console.ReadLine();
+            Console.Write("Salario :");
+            A1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite os dados do segundo funcionario :");
+            Console.Write("Nome :");
+            A2.Nome = Console.ReadLine();
+            Console.Write("Salario :");
+            A2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double MediaSalario = (A1.Salario + A2.Salario) / 2.0;
+
+            Console.WriteLine("Salario médio = R$" + MediaSalario.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}

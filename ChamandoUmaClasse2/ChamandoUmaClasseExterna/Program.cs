9f4b485f76ea8664820ec7f88e6169084565e6dd
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-09 22:22:15 -0300

    Add files via upload

---
+++ ChamandoUmaClasse2/ChamandoUmaClasseExterna/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ChamandoUmaClasseExterna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoas p1 = new Pessoas();
+            Pessoas p2 = new Pessoas();
+
+            Console.WriteLine("Digita os dados da primeira pessoa: ");
+            Console.Write("Nome : ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade : ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digita os dados da segunda pessoa: ");
+            Console.Write("Nome : ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade : ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine("A primeira pessoa é mais velha: ");
+            } else
+            {
+                Console.WriteLine("A segunda pessoa é mais velha:");
+            }
+        }
+    }
+}

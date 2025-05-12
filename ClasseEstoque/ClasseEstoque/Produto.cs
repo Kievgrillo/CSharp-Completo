4cd50531f074102859dae8c259bca0481a8cbdc4
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-12 17:01:52 -0300

    Add files via upload
    fazendo a inicializaçãod de VAR dentro de métodos, melhor forma de concatenação sem poluir o programa principal, uso de TOSTRING() na conversão de VAR, chamada de método.

---
+++ ClasseEstoque/ClasseEstoque/Produto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ClasseEstoque
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        // var de parametro de entrada de funcao "(int quantidade)"
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome + ", $" + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}

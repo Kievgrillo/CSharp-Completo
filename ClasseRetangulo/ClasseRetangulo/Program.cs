24ca4f380be94cd8846a9459411c6e9b432fd5f1
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-12 21:56:04 -0300

    Add files via upload

---
+++ ClasseRetangulo/ClasseRetangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClasseRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo L = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retângulo: ");
+            L.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            L.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area = " + L.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro = " + L.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + L.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ ClasseRetangulo/ClasseRetangulo/Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseRetangulo
+{
+    public class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return (Largura + Altura) * 2.0;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}

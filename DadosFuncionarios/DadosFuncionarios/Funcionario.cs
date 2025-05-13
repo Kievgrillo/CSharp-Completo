11fa752c40718614fb04874a76aef72e7c5c6147
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-12 22:25:16 -0300

    Add files via upload

---
+++ DadosFuncionarios/DadosFuncionarios/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace DadosFuncionarios
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto + porcentagem / 100);
+        }
+
+        //sobreposicao para fornecer o valor atual do parametro salario
+        public override string ToString() 
+        {
+            return Nome + ", R$ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ DadosFuncionarios/DadosFuncionarios/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System;
+
+namespace DadosFuncionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario F = new Funcionario();
+
+            Console.WriteLine("Nome :");
+            F.Nome = Console.ReadLine();
+            Console.WriteLine("Salario Bruto :");
+            F.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Imposto :");
+            F.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Funcionário :" + F);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a porcetagem para aumentar o salário: ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            F.AumentarSalario(porcent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + F.Nome + F.SalarioBruto);
+        }
+    }
+
+}

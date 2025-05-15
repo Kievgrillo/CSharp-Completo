82dbde043728418edc337150839e77ba2af768a0
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-15 10:16:50 -0300

    Add files via upload
    Permitir que os mebros da classe calculadora sejam chamados sem precisar instanciar o objeto no programa principal. Chamando a classe junto com o método.

---
+++ FuncaoEstatica2/FuncaoEstatica2/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace FuncaoEstatica2
+{
+    class Calculadora
+    {
+        public static double Pi = 3.14;
+
+        public static double circunferencia(double R)
+        {
+            return 2.0 * Pi * R;
+        }
+        public static double Volume(double R)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(R, 3.0);
+        }
+    }
+}
---
+++ FuncaoEstatica2/FuncaoEstatica2/Program.cs
@@ -0,0 +1,22 @@
+﻿using FuncaoEstatica2;
+using System;
+using System.Globalization;
+
+namespace FuncaoEstatica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

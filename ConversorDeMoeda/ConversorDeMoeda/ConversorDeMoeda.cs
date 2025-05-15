e3548a2bdd0f43e0936929e32c4eb3624c6569c6
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-15 11:36:10 -0300

    Add files via upload

---
+++ ConversorDeMoeda/ConversorDeMoeda/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿namespace ConversorDeMoeda
+{
+    class ConversorDeMoeda
+    {
+        public static double ConverterDolarParaReal(double cotacaoDolar, double quantidadeDolar)
+        {
+            double valorSemIOF = cotacaoDolar * quantidadeDolar;
+            double iof = valorSemIOF * 0.06; // 6% de IOF
+            return valorSemIOF + iof;
+        }
+    }
+}
+
---
+++ ConversorDeMoeda/ConversorDeMoeda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorDeMoeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantidadeDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valorEmReais = ConversorDeMoeda.ConverterDolarParaReal(cotacaoDolar, quantidadeDolar);
+
+            Console.WriteLine("Valor a ser pago em reais = " + valorEmReais.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

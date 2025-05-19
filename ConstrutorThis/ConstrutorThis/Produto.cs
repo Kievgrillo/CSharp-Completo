de28da0810da0f2abd061cf363f1d57b8bba964b
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-19 11:27:55 -0300

    Add files via upload
    Usando herança com a palavra THIS, mas aplicando no construtor da classe.

---
+++ ConstrutorThis/ConstrutorThis/Produto.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+
+namespace UsandoConstrutor
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto()
+        {
+            Quantidade = 5;
+        }
+        public Produto(string nome, double preco) : this()
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            Quantidade = quantidade;
+        }
+
+        
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        // var de parametro de entrada de funcao "(int quantidade)"
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome + ", $" + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}

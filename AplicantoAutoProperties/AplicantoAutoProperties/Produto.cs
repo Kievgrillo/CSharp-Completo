e4b5ee1dcf7d31236d0c23e0f1a12bbac0b689e5
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-21 23:52:16 -0300

    Add files via upload
    Aplicanto auto propriedade e seguindo um tipo de estruturação de código no c#

---
+++ AplicantoAutoProperties/AplicantoAutoProperties/Produto.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+
+namespace AplicantoAutoProperties
+{
+    class Produto
+    {
+        //atributos privados
+        private string _nome;
+
+        //propriedade autoimplementada
+        public double Preco { get; private set; }
+        public double Quantidade { get; set; }
+
+        //construtores
+        public Produto()
+        {
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //propriedade customizadas
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        //metodos da classe
+        public double ValorTotalEmEstoque
+        {
+            get { return Preco * Quantidade; }
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
+

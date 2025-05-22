8e38f64c01c53d56442d93dd545d818283ef2dae
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-21 23:43:56 -0300

    Add files via upload
    Aplicanto propriedade, outra melhoria que é usada no encapsulamento.

---
+++ AplicandoProperties/AplicandoProperties/Program.cs
@@ -0,0 +1,19 @@
+﻿using AplicantoProperties;
+using System;
+using System.Globalization;
+
+namespace AplicantoProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+        }
+    }
+}

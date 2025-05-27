3bdd53cd36c7998764dbb56fb9fb1223023809a9
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-27 15:55:52 -0300

    Add files via upload

---
+++ Vetor/Vetor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                soma += vet[i];
+            }
+
+            double avg = soma / n;
+
+            Console.WriteLine("Alura media = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

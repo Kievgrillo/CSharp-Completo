0a731647f56a63086f6da195b50c4e59776c9b06
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-05-27 16:24:22 -0300

    Add files via upload

---
+++ VetorTipoClasse/VetorTipoClasse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using VetorTipoClasse;
+
+namespace Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] vet = new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vet[i] = new Produto(name, preco);
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                soma += vet[i].Preco;
+            }
+
+            double avg = soma / n;
+
+            Console.WriteLine("Preço Medio = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

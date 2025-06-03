2b6a0a85dfb18cbda0e5d4d9161e252c18c14f5e
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-06-03 09:18:08 -0300

    Add files via upload

---
+++ AtividadeVetorFixacao/AtividadeVetorFixacao/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AtividadeVetorFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vet = new Estudante[10];
+
+            Console.WriteLine("Quantos quartos serao alugados ? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.WriteLine("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vet[quarto] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados");
+            for(int i = 0;i < vet.Length;i++)
+            {
+                if (vet[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vet[i]);
+                }
+            }
+        }
+    }
+}

a647feae122da029df5a38de063c2c86a623fa39
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-06-07 14:57:25 -0300

    Add files via upload

---
+++ AtividadeComLista/AtividadeComLista/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace AtividadeComLista
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Pagamento { get; private set; }
+
+        public Funcionario(int id, string nome, double pagamento)
+        {
+            Id = id;
+            Nome = nome;
+            Pagamento = pagamento;
+        }
+
+        public void Salario(double porcentagem)
+        {
+            Pagamento += Pagamento + porcentagem / 100.00;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Nome + ", " + Pagamento.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ AtividadeComLista/AtividadeComLista/Program.cs
@@ -0,0 +1,52 @@
+﻿using AtividadeComLista;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace AtividaeComLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Quantos funcionários serão registrados ?");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Funcionario> list = new List<Funcionario>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Funcionaroio #" + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome :");
+                string nome = Console.ReadLine();
+                Console.Write("Salário :");
+                double pagamento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Funcionario(id, nome, pagamento));
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Entre com ID do funcionário que terá o salário aumentado :");
+            int procurarId = int.Parse(Console.ReadLine());
+
+            Funcionario func = list.Find(x => x.Id == procurarId);
+            if (func != null)
+            {
+                Console.WriteLine("Entre com porcentagem :");
+                double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                func.Salario(porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("Id nao existe");
+            }
+            Console.WriteLine("Lista atualizada de funcionários");
+            foreach (Funcionario obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}

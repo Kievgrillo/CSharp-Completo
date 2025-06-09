09eed6634a0792dd518810c48c60e12768049f6c
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-06-09 12:12:49 -0300

    Add files via upload
    Expressão condicional ternária, podendo substituir alguns IFs dependendo do que está sendo pedido, otimizando o código e deixando mais limpo.

---
+++ ExpressaoCondicionalTernaria/ExpressaoCondicionalTernaria/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExpressaoCondicionalTernaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //isso substitui um IF dependendo do caso, mais perfomance para o projeto.
+            double desconto = (preco < 20.0) ? preco * 0.15 : preco * 0.10;
+
+            Console.WriteLine(desconto);
+        }
+    }
+}

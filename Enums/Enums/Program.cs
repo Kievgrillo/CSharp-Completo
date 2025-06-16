524fd7822377f3207dad29ac5b7f528231f8aba5
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-06-16 12:06:19 -0300

    Add files via upload
    Fazendo a aplicação de Enums e Entities numa classe que visa o status do pedido.

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,29 @@
+﻿using Enums.Entities;
+using Enums.Entities.Enums;
+using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 100,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PagamentoPendente
+            };
+
+            Console.WriteLine(order);
+
+            //trasnformando um tipo enumerado para tipo string
+            string txt = OrderStatus.PagamentoPendente.ToString();
+            Console.WriteLine(txt);
+
+            //transformando um tipo string para um tipo enumerado
+            OrderStatus OS = Enum.Parse<OrderStatus>("Entregue");
+            Console.WriteLine(OS);
+        }
+    }
+}

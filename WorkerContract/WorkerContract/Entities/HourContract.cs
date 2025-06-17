ac27dc9fb55d86ca205c1e8b49dc82258a050291
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-06-17 13:02:09 -0300

    Add files via upload
    Solidificando a parte de entidades do C#, pegando um diagrama de UML, interpretando as classes e metodos do diagrama e trasnformando em codigo

---
+++ WorkerContract/WorkerContract/Entities/HourContract.cs
@@ -0,0 +1,25 @@
+﻿namespace WorkerContract.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePorHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valuePorHour, int hours)
+        {
+            Date = date;
+            ValuePorHour = valuePorHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePorHour;
+        }
+    }
+}
---
+++ WorkerContract/WorkerContract/Entities/Worker.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.Contracts;
+using WorkerContract.Entities.Enums;
+
+namespace WorkerContract.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public double BaseSalary { get; set; }
+        public WorkerLevel Level { get; set; }
+
+        //propriedade chamada contract para receber uma lista de contratos do trabalhador, mas ela n pode ser vazia, pois o trabalhador pode receber varias listas 
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        //Associacao de duas classes diferentes 
+        public Department Department { get; set; }
+
+        public Worker()
+        {
+        }
+        public Worker(string name, double baseSalary, WorkerLevel level, Department department)
+        {
+            Name = name;
+            BaseSalary = baseSalary;
+            Level = level;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double soma = BaseSalary;
+
+            //foreach para percorrer a lista de contratos
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    soma += contract.TotalValue();
+                }
+            }
+
+            return soma;
+        }
+
+    }
+}

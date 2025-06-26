8f6e0810e67f9090273412ae3102e9ee90564c90
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-06-26 10:04:02 -0300

    Add files via upload
    revisando o método de herança, usando a sintaxe ":" para herdar a classe para uma subclasse, também usando o "base" no construtor para herdar os atributos do construtor da classe principal na subclasse, usando o método protected para proteger um determinado dado e colocar um encapsulamento básico, usando o private no método set, protegendo a alteração.

---
+++ Heranca1/Heranca1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Heranca1.Entities;
+
+namespace Heranca1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "maria", 100.00, 500);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}

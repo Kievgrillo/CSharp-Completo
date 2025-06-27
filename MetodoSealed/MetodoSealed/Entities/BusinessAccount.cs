f0590dce88927a60e0ea62197ba809932d78610c
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-06-27 10:16:10 -0300

    Add files via upload
    método sealed, podendo ser usado tanto na classe do projeto ou em alguum método , tendo algumas vantagens dependendo da regra de negócio para segurança e perfomance em tempo de execução.

---
+++ MetodoSealed/MetodoSealed/Entities/BusinessAccount.cs
@@ -0,0 +1,23 @@
+﻿namespace Heranca1.Entities
+{
+    public class BusinessAccount : Account
+    {
+        public double loanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+        public BusinessAccount(int number, string holder, double balance, double loanlimit) : base(number, holder, balance)
+        {
+            loanLimit = loanlimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= loanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ MetodoSealed/MetodoSealed/Entities/SavingsAccount.cs
@@ -0,0 +1,31 @@
+﻿using Heranca1.Entities;
+
+namespace Course.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        //esse metodo sealed serve para o metodo nao ser inscrito novamente em uma outra subclasse, deixando o metodo selado.
+        public sealed override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}

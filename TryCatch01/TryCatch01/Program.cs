7aabb5cf62da0bdc27d6244b757d4d528e5d34b9
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-07-23 08:49:33 -0300

    Add files via upload
    Trabalhando com try catch, entendendo os tipos de erros que podemos evidenciar na hora de fazer um método para facilitar a manuntenção ou implementação

---
+++ TryCatch01/TryCatch01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TryCatch01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int resultado = n1 / n2;
+                Console.WriteLine(resultado);
+            }
+            catch (DivideByZeroException) 
+            {
+                Console.WriteLine("Division by zero is not allowed");
+            } 
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format erro! " + e.Message);
+            }
+        }
+    }
+}

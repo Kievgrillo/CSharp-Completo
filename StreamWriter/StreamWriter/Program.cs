68e93c81cfbf8ce950cf4c880a75fbed47715149
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-08-08 16:19:44 -0300

    Add files via upload

---
+++ StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StreamWriterr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcerPatch = @"C:\Users\Keven\test.txt";
+            string TargetPatch = @"C:\Users\Keven\test2.txt";
+
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(sourcerPatch);
+
+                using (StreamWriter sw = File.AppendText(TargetPatch))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}

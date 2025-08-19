ec286abe67cd9508f9d67f9ab5bc1b370e699378
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-08-18 22:21:40 -0300

    Add files via upload
    PT-BR Criando um projeto com interface, fazendo injenção de dependência e começando a trabalhar os principíos do SOLID em C#
    
    EN Creating a project with an interface, doing dependency injection and starting to work on the principles of SOLID in C#

---
+++ AluguelComInterface/AluguelComInterface/Services/ITaxService.cs
@@ -0,0 +1,7 @@
+﻿namespace AluguelComInterface.Services
+{
+    interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
---
+++ AluguelComInterface/AluguelComInterface/Services/RentalService.cs
@@ -0,0 +1,37 @@
+﻿using AluguelComInterface.Entities;
+
+namespace AluguelComInterface.Services
+{
+    internal class RentalService
+    {
+        public double PricePorHour { get; set; }
+        public double PricePerDay { get; set; }
+
+        private ITaxService _taxService;
+
+        public RentalService(double pricePorHour, double pricePerDay, ITaxService taxService)
+        {
+            PricePorHour = pricePorHour;
+            PricePerDay = pricePerDay;
+            _taxService = taxService;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start); 
+
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePorHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+            double tax = _taxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}

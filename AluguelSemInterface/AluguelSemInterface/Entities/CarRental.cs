8be4561afecd178e35ac3d202f8776d17cc6f182
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-08-18 22:20:18 -0300

    Add files via upload
    PT-BR Implementando uma solução sem interface, com variáveis fortemente tipada, envolvendo a criação de entidades e serviços no projeto.
    
    EN interface creation in solution, with strongly typed variables, involving the creation of entities and services in the project.

---
+++ AluguelSemInterface/AluguelSemInterface/Entities/CarRental.cs
@@ -0,0 +1,19 @@
+﻿namespace AluguelSemInterface.Entities
+{
+     class CarRental
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+
+        //fazendo associacao de classes
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+        }
+    }
+}
---
+++ AluguelSemInterface/AluguelSemInterface/Services/ITaxService.cs
@@ -0,0 +1,7 @@
+﻿namespace AluguelSemInterface.Services
+{
+    interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}

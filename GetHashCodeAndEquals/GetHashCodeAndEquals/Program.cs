0f3d006fb27f54dc054e951749e804675c949548
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-10-20 08:53:34 -0300

    Add files via upload

---
+++ GetHashCodeAndEquals/GetHashCodeAndEquals/Program.cs
@@ -0,0 +1,18 @@
+﻿using GetHashCodeAndEquals.Entities;
+using System;
+
+namespace GetHashCodeAndEquals
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "Maria@gmail.com" };
+            Client b = new Client { Name = "Joao", Email = "joao@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}

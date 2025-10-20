dcf97a3a900e7ed497bfc5bf3b1bcbe6f14edaa5
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-10-20 08:54:08 -0300

    Add files via upload

---
+++ DiferenceGetHashCodeForValueAndReference/DiferenceGetHashCodeForValueAndReference/Program.cs
@@ -0,0 +1,29 @@
+﻿using DiferenceGetHashCodeForValueAndReference.Entities;
+using System;
+using System.Drawing;
+using Point = System.Drawing.Point;
+
+namespace DiferenceGetHashCodeForValueAndReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.00));
+            a.Add(new Product("Notebook", 1900.00));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product p = new Product("Notebook", 1900.00);
+            Console.WriteLine(a.Contains(p));
+
+            Point p2 = new Point(5, 10);
+            Console.WriteLine(b.Contains(p2));
+
+
+        }
+    }
+}

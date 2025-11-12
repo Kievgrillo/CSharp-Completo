2ee30838ce154903bb8a979a3320f3cc4b009115
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-11-11 23:47:11 -0300

    Add files via upload
    en -> Getting started with Linq
     
    pt-br -> Intrudução ao LINQ

---
+++ ApplicationLinq/ApplicationLinq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ApplicationLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //specify the data source
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            //define the query expression
+            IEnumerable<int> result = numbers
+               .Where(n => n % 2 == 0)
+               .Select(n => n * n);
+            //Execute the query
+            foreach (int n in result)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
